52f180ed02615bee604c796498b383c94cd3ca30
Jon Ziebell <ziebelje@gmail.com>
2020-01-16 18:53:54 -0500

    Removing old analytics code

--- js/js.php
+++ js/js.php
@@ -27,7 +27,6 @@ if($setting->get('environment') === 'dev' || $setting->get('environment') === 'd
   echo '<script src="/js/beestat/time.js"></script>' . PHP_EOL;
   echo '<script src="/js/beestat/setting.js"></script>' . PHP_EOL;
   echo '<script src="/js/beestat/poll.js"></script>' . PHP_EOL;
-  echo '<script src="/js/beestat/google_analytics.js"></script>' . PHP_EOL;
   echo '<script src="/js/beestat/home_comparisons.js"></script>' . PHP_EOL;
   echo '<script src="/js/beestat/highcharts.js"></script>' . PHP_EOL;
   echo '<script src="/js/beestat/get_sync_progress.js"></script>' . PHP_EOL;
