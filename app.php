ce3f9220881487eed93498dbe4877b6edbf58c6a
Jon Ziebell <ziebelje@gmail.com>
2020-02-11 19:19:06 -0500

    Fixed #238 - Add query string to CSS and manifest

--- app.php
+++ app.php
@@ -1,5 +1,8 @@
 <?php
 
+  require_once 'api/cora/setting.php';
+  $setting = cora\setting::get_instance();
+
   // If you're not logged in, just take you directly to the ecobee login page.
   if(isset($_COOKIE['session_key']) === false) {
     header('Location: http://' . $_SERVER['HTTP_HOST'] . '/api/?resource=ecobee&method=authorize&arguments={}&api_key=ER9Dz8t05qUdui0cvfWi5GiVVyHP6OB8KPuSisP2');
@@ -16,10 +19,15 @@
     <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
     <!-- Chrome, Firefox OS and Opera -->
     <meta name="theme-color" content="#222222">
-    <link rel="manifest" href="/manifest.json">
+    <?php
+      require_once 'api/cora/setting.php';
+      $setting = cora\setting::get_instance();
 
-    <link href="../css/dashboard.css" rel="stylesheet">
-    <?php require 'js/js.php'; ?>
+      echo '<link rel="manifest" href="/manifest.json?' . $setting->get('commit') . '">';
+      echo '<link href="../css/dashboard.css?' . $setting->get('commit') . '" rel="stylesheet">';
+
+      require 'js/js.php';
+    ?>
   </head>
   <body></body>
 </html>
