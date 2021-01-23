d9fd07618af1f00797c01e5bc32d1997cf29ee54
Jon Ziebell <ziebelje@gmail.com>
2021-01-23 13:46:21 -0500

    Added documentation to cache

--- js/beestat/cache.js
+++ js/beestat/cache.js
@@ -2,6 +2,12 @@ beestat.cache = {
   'data': {}
 };
 
+/**
+ * Overwrite a cache key with new data. Dispatches an event when done.
+ *
+ * @param {string} key The cache key to update.
+ * @param {object} value The data to be placed in that key.
+ */
 beestat.cache.set = function(key, value) {
   if (key.substring(0, 5) === 'data.') {
     beestat.cache.data[key.substring(5)] = value;
@@ -11,6 +17,11 @@ beestat.cache.set = function(key, value) {
   beestat.dispatcher.dispatchEvent('cache.' + key);
 };
 
+/**
+ * Delete data from the cache. Dispatches an event when done.
+ *
+ * @param {string} key The cache key to delete.
+ */
 beestat.cache.delete = function(key) {
   if (key.substring(0, 5) === 'data.') {
     delete beestat.cache.data[key.substring(5)];
