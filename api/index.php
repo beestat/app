0f7b1c28a7594b7a2328f85065eb21bd51d4378e
Jon Ziebell <ziebelje@gmail.com>
2021-04-17 20:33:21 -0400

    Added maintenance notification

--- api/index.php
+++ api/index.php
@@ -1,5 +1,7 @@
 <?php
 
+die('Down for some brief maintenance. Back soon!');
+
 /**
  * Entry point for the API. This sets up cora, the error/exception handlers,
  * and then sends the request off for processing. All requests should start
