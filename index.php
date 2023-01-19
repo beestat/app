d4350d8a905c8ae45994fa5b22d009789e5ae06b
Jon Ziebell <ziebelje@gmail.com>
2023-01-18 21:15:02 -0500

    Removed some deprecated public website files

--- index.php
+++ index.php
@@ -20,9 +20,34 @@
     $_COOKIE['session_key'] = 'd31d3ef451fe65885928e5e1bdf4af321f702009';
   }
 
-  // Skip this page entirely if you're logged in.
-  if($setting->is_demo() === true || preg_match('/app\.beestat\.io/', $_SERVER['HTTP_HOST']) !== 0) {
-    require 'app.php';
-  } else {
-    require 'www.php';
+  require_once 'api/cora/setting.php';
+  $setting = cora\setting::get_instance();
+
+  // If you're not logged in, just take you directly to the ecobee login page.
+  if(isset($_COOKIE['session_key']) === false) {
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/api/?resource=ecobee&method=authorize&arguments={}&api_key=ER9Dz8t05qUdui0cvfWi5GiVVyHP6OB8KPuSisP2');
+    die();
   }
+
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>beestat</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#222222">
+    <!-- Icon for pinning on iOS -->
+    <link rel="apple-touch-icon" href="/favicon_apple.png">
+    <?php
+
+      echo '<link rel="manifest" href="/manifest.json?' . $setting->get('commit') . '">';
+      echo '<link href="../css/dashboard.css?' . $setting->get('commit') . '" rel="stylesheet">';
+
+      require 'js/js.php';
+    ?>
+  </head>
+  <body></body>
+</html>
