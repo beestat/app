29166179c445abf55dbb608caeba1ab082bfcd59
Jon Ziebell <ziebelje@gmail.com>
2026-02-14 16:52:26 -0500

    Skeleton

--- js/lib/straight-skeleton/wrapper.js
+++ js/lib/straight-skeleton/wrapper.js
@@ -1,5 +1,10 @@
-// Import the straight-skeleton library from CDN
-import { SkeletonBuilder } from 'https://esm.sh/straight-skeleton@2.0.1';
+// Load the straight-skeleton library from a local file.
+import '/js/lib/straight-skeleton/index.js';
+
+const SkeletonBuilder = window.SkeletonBuilder;
+if (SkeletonBuilder === undefined) {
+  throw new Error('Failed to load local straight-skeleton runtime');
+}
 
 // Initialize the WASM module
 await SkeletonBuilder.init();
